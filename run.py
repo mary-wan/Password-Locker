cb3361fe9e84a8004d454dc4ffdbf0c5bea1174e
mary-wan <mary.njenga@student.moringaschool.com>
2021-10-22 20:17:21 +0300

    change logo color

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3.6
 from user import User
 from credentials import Credential
+from termcolor import colored
+
 
 def logo():
-	print("               ____                      _                  _         ")
-	print("              |  _ \                    | |                | |  _     ")
-	print("              | |_) )  ____  ___   ___  | |     ___    ___ | | / /    ")
-	print("              |  __/  / _  |/ __  / __  | |    / _  \ /  _ | |/ /     ")
-	print("              | |    / (_| |\__ \ \__ \ | |___| (_)  |  (_ |   \      ")
-	print("              |_|    \_____| ___/  ___/ |_____|\ ___/ \___ |_|\_\     ")
+	print(colored("        ____                      _                  _         ","green"))
+	print(colored("       |  _ \                    | |                | |  _     ","green"))
+	print(colored("       | |_) )  ____  ___   ___  | |     ___    ___ | | / /    ","green"))
+	print(colored("       |  __/  / _  |/ __  / __  | |    / _  \ /  _ | |/ /     ","green"))
+	print(colored("       | |    / (_| |\__ \ \__ \ | |___| (_)  |  (_ |   \      ","green"))
+	print(colored("       |_|    \_____| ___/  ___/ |_____|\ ___/ \___ |_|\_\     ","green"))
+	print("\n")
  
 logo()
+
+# text = colored('Hello, World!', 'red')
