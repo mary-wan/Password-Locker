4b11b9d69f4e1593d8e39ee02c34e7cdbacfd44a
mary-wan <mary.njenga@student.moringaschool.com>
2021-10-22 19:54:21 +0300

    add logo

---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.6
+from user import User
+from credentials import Credential
+
+def logo():
+	print("               ____                      _                  _         ")
+	print("              |  _ \                    | |                | |  _     ")
+	print("              | |_) )  ____  ___   ___  | |     ___    ___ | | / /    ")
+	print("              |  __/  / _  |/ __  / __  | |    / _  \ /  _ | |/ /     ")
+	print("              | |    / (_| |\__ \ \__ \ | |___| (_)  |  (_ |   \      ")
+	print("              |_|    \_____| ___/  ___/ |_____|\ ___/ \___ |_|\_\     ")
+ 
+logo()
