459cab490550a67399b7dccf23e5f9393545ebb5
mary-wan <mary.njenga@student.moringaschool.com>
2021-10-22 18:12:42 +0300

    add user save and verification methods

--- user.py
+++ user.py
@@ -5,6 +5,20 @@ class User:
     
     user_list=[]
     
-   
+    def __init__(self, username,password):
+        self.username =username
+        self.password =  password
+        
+    def user_save(self):
+        '''
+        method to login user
+        '''
+        User.user_list.append(self)
+    
+    @classmethod
+    def user_verification(cls,username,password):
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+              return username
        
         
