0d531ff32ad0e57f386436caed4f86fa0b0ef40d
mary-wan <mary.njenga@student.moringaschool.com>
2021-10-22 18:48:21 +0300

    add test for save and verify user

--- user.py
+++ user.py
@@ -9,16 +9,19 @@ class User:
         self.username =username
         self.password =  password
         
-    def user_save(self):
+    def save_user(self):
         '''
-        method to login user
+        method to login user object to user list
         '''
         User.user_list.append(self)
     
     @classmethod
     def user_verification(cls,username,password):
+        '''
+        method to login user credentials on login
+        '''
         for user in User.user_list:
             if(user.username == username and user.password == password):
-              return username
-       
+             return user
+        
         
